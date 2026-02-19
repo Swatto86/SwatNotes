d9944451701b009d8b956aa76b1bb32fe9d3950a
Swatto <58915441+Swatto86@users.noreply.github.com>
2026-02-19 12:25:40 +0000

    chore: bump version to 2.0.9

--- src/config.ts
+++ src/config.ts
@@ -36,5 +36,5 @@ export const DEFAULT_NOTE_CONTENT = JSON.stringify({ ops: [{ insert: '\n' }] });
 export const DEFAULT_NOTE_TITLE = 'Untitled';
 
 // Application info
-export const APP_VERSION = '2.0.8';
+export const APP_VERSION = '2.0.9';
 export const APP_NAME = 'SwatNotes';
